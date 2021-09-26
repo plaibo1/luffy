9cc5dd9928b0c68dfcfd92565b6d4a3d66cffeaa
plaibo1 <=>
2021-09-26 13:28:11 +0300

    microUpdate

--- src/Redux/musicPage-reducer.js
+++ src/Redux/musicPage-reducer.js
@@ -3,7 +3,7 @@ let initialState = {
     musicData: [
         {id: 1, artist: 'Lil peep', song: 'Viens', img: "https://tntmusic.ru/media/content/article/2019-11-19_11-43-20__c8c75a3a-0ac1-11ea-9fd6-d1f808b6ab45.jpg"},
         {id: 2, artist: 'Lil peep', song: 'Lil Kennedy', img: "https://tntmusic.ru/media/content/article/2019-11-19_11-43-20__c8c75a3a-0ac1-11ea-9fd6-d1f808b6ab45.jpg"},
-        {id: 3, artist: 'Lil peep', song: 'Benz Truck pt.2', img: "https://tntmusic.ru/media/content/article/2019-11-19_11-43-20__c8c75a3a-0ac1-11ea-9fd6-d1f808b6ab45.jpg"},
+        {id: 3, artist: 'Lil peepпгдз', song: 'Benz Truck pt.2', img: "https://tntmusic.ru/media/content/article/2019-11-19_11-43-20__c8c75a3a-0ac1-11ea-9fd6-d1f808b6ab45.jpg"},
       ]
 }
 
